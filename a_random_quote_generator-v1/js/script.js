8a01a74bd0a235e4cb43558e9982708cf712f762
prjmayhem <csschaff@gmail.com>
2022-09-07 16:45:29 -0700

    new
    new

---
+++ a_random_quote_generator-v1/js/script.js
@@ -0,0 +1,46 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array change 3333
+ ***/
+
+
+
+/***
+ * `getRandomQuote` function
+ * 
+ * function getRandomQuote(arr){
+ *  randomNumber = Math.floor(Math.random() * x + 1)
+ *  array = quotes[randomNumber];
+ *  return array;
+ * 
+ * }
+ ***/
+
+
+
+/***
+ * `printQuote` function
+ * 
+ * 
+ * function pringQuote(){
+ *  quotee = getRandomQuote() ;
+ *  return quote;
+ * }
+ ***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
